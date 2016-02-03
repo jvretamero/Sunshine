7896de307097b2c5b23e23a6c407083cb35b5727
João Vitor <joaovretamero@gmail.com>
2016-02-03 19:28:02 -0200

    ArrayAdapter para a ListView

--- app/src/main/java/com/joaoretamero/sunshine/MainActivityFragment.java
+++ app/src/main/java/com/joaoretamero/sunshine/MainActivityFragment.java
@@ -1,10 +1,15 @@
 package com.joaoretamero.sunshine;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivityFragment extends Fragment {
 
@@ -14,6 +19,20 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        List<String> listaClimaSemana = new ArrayList<String>();
+        listaClimaSemana.add("Hoje - Sol - 35/25");
+        listaClimaSemana.add("Amanhã - Chuva - 20/10");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getActivity(),
+                R.layout.list_item_forecast,
+                R.id.list_item_forecast_textview,
+                listaClimaSemana);
+
+        ListView listForecast = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listForecast.setAdapter(arrayAdapter);
+
+        return rootView;
     }
 }
