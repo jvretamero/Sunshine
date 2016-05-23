a174111a325c1f4961e1cb63f9d975756f653ce5
João Vitor <joaovretamero@gmail.com>
2016-05-23 20:58:01 -0300

    Finalizado a tela de configuração

--- app/src/main/java/com/joaoretamero/sunshine/MainActivity.java
+++ app/src/main/java/com/joaoretamero/sunshine/MainActivity.java
@@ -1,12 +1,18 @@
 package com.joaoretamero.sunshine;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +34,28 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
             startActivity(intent);
             return true;
+        } else if (id == R.id.action_map) {
+            openPreferredLocationInMap();
+            return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void openPreferredLocationInMap() {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+        String location = sharedPreferences.getString(
+                getString(R.string.pref_location_key),
+                getString(R.string.pref_location_default_value));
+
+        Uri geoUri = Uri.parse("geo:0,0?").buildUpon().appendQueryParameter("q", location).build();
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoUri);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d(TAG, "openPreferredLocationInMap: Não foi possível exibir no mapa (" + location + ")");
+        }
     }
 }
--- app/src/main/java/com/joaoretamero/sunshine/SettingsActivity.java
+++ app/src/main/java/com/joaoretamero/sunshine/SettingsActivity.java
@@ -1,7 +1,6 @@
 package com.joaoretamero.sunshine;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
@@ -10,8 +9,8 @@ import android.preference.PreferenceManager;
 public class SettingsActivity extends PreferenceActivity implements Preference.OnPreferenceChangeListener {
 
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.pref_general);
         bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_location_key)));
         bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_unit_key)));
@@ -33,7 +32,7 @@ public class SettingsActivity extends PreferenceActivity implements Preference.O
             ListPreference listPreference = (ListPreference) preference;
             int preferenceIndex = listPreference.findIndexOfValue(stringValue);
             if (preferenceIndex >= 0)
-                preference.setSummary(stringValue);
+                preference.setSummary(listPreference.getEntries()[preferenceIndex]);
         } else {
             preference.setSummary(stringValue);
         }
