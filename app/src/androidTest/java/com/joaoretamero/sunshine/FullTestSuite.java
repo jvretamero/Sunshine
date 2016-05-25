0bc71b49d5a355c9a1f2752edc3dd112946c7a34
João Vitor <joaovretamero@gmail.com>
2016-05-24 22:27:21 -0300

    Adição dos testes; Criação do DBHelper

--- app/src/androidTest/java/com/joaoretamero/sunshine/FullTestSuite.java
+++ app/src/androidTest/java/com/joaoretamero/sunshine/FullTestSuite.java
@@ -1,4 +1,17 @@
 package com.joaoretamero.sunshine;
 
-public class FullTestSuite {
+import android.test.suitebuilder.TestSuiteBuilder;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class FullTestSuite extends TestSuite {
+    
+    public FullTestSuite() {
+        super();
+    }
+
+    public static Test suite() {
+        return new TestSuiteBuilder(FullTestSuite.class).includeAllPackagesUnderHere().build();
+    }
 }
--- app/src/main/java/com/joaoretamero/sunshine/data/WeatherContract.java
+++ app/src/main/java/com/joaoretamero/sunshine/data/WeatherContract.java
@@ -26,11 +26,11 @@ public class WeatherContract {
         public static final String COLUMN_DATE = "date";
         public static final String COLUMN_WEATHER_ID = "weather_id";
         public static final String COLUMN_SHORT_DESC = "short_desc";
-        public static final String COLUMN_MIN_TEMPO = "min";
+        public static final String COLUMN_MIN_TEMP = "min";
         public static final String COLUMN_MAX_TEMP = "max";
         public static final String COLUMN_HUMIDITY = "humidity";
         public static final String COLUMN_PRESSURE = "pressure";
-        public static final String COLUMN_WIND = "wind";
+        public static final String COLUMN_WIND_SPEED = "wind";
         public static final String COLUMN_DEGREES = "degrees";
     }
 }
